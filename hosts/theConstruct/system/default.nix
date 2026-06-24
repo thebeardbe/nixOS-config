a22db3ded450219850c88cd90fa091e18ea2c6f4
TheBeardBE <bunker@achter.be>
2026-06-24 22:16:20 +0200

    feat: add exfat drives auto-mount for theConstruct

--- hosts/theConstruct/system/default.nix
+++ hosts/theConstruct/system/default.nix
@@ -19,4 +19,31 @@
       useOSProber = true;
     };
   };
+
+  # exfat support
+  boot.supportedFilesystems = [ "exfat" ];
+
+  # Storage drives — auto-mount at boot
+  fileSystems = {
+    "/mnt/golden-city" = {
+      device = "/dev/disk/by-uuid/B89B-399D";
+      fsType = "exfat";
+      options = [ "uid=1000" "gid=100" "fmask=0022" "dmask=0022" ];
+    };
+    "/mnt/blue-fire" = {
+      device = "/dev/disk/by-uuid/8CB1-7A97";
+      fsType = "exfat";
+      options = [ "uid=1000" "gid=100" "fmask=0022" "dmask=0022" ];
+    };
+    "/mnt/black-glass" = {
+      device = "/dev/disk/by-uuid/32CA-F4E4";
+      fsType = "exfat";
+      options = [ "uid=1000" "gid=100" "fmask=0022" "dmask=0022" ];
+    };
+    "/mnt/silver-light" = {
+      device = "/dev/disk/by-uuid/BAE0-0704";
+      fsType = "exfat";
+      options = [ "uid=1000" "gid=100" "fmask=0022" "dmask=0022" ];
+    };
+  };
 }
