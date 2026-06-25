f3cb9884dc751cce853bf41045b14687a7a7e477
TheBeardBE <bunker@achter.be>
2026-06-25 10:35:14 +0200

    chore: switch hyprland config to lua

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -14,7 +14,7 @@ in
 {
   wayland.windowManager.hyprland = {
     enable = true;
-    configType = "hyprlang";
+    configType = "lua";
     settings = {
       # --- Variables ---
       "$mod" = "SUPER";
