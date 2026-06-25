0b64cdde540160d8b329f768dabab31a1c0a5a5b
TheBeardBE <bunker@achter.be>
2026-06-25 14:33:27 +0200

    fix: smaller GTK font on theConstruct (affects Firefox)

--- hosts/theConstruct/home/default.nix
+++ hosts/theConstruct/home/default.nix
@@ -4,6 +4,12 @@
     ./monitors.nix
   ];
 
+  # Smaller GTK font for the ultrawide (affects Firefox UI, etc.)
+  gtk.font = lib.mkForce {
+    name = "FiraCode Nerd Font";
+    size = 10;
+  };
+
   # Smaller kitty font on the big ultrawide
   programs.kitty.font.size = lib.mkForce 9;
   programs.waybar.settings.mainBar.modules-right = lib.mkForce [
