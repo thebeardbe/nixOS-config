0a30067350374344ca6db285d3542fa637e11291
TheBeardBE <bunker@achter.be>
2026-06-25 10:12:29 +0200

    fix: flatten greetd command to single line to avoid TOML parsing issues

--- common/configuration.nix
+++ common/configuration.nix
@@ -204,15 +204,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = ''
-          ${pkgs.tuigreet}/bin/tuigreet \
-            --time \
-            --remember \
-            --user-menu \
-            --asterisks \
-            --theme 'border=blue;text=blue;prompt=blue;time=blue;action=blue;button=blue;container=black;input=blue' \
-            --cmd Hyprland
-        '';
+        command = ''${pkgs.tuigreet}/bin/tuigreet --time --remember --user-menu --asterisks --theme 'border=blue;text=blue;prompt=blue;time=blue;action=blue;button=blue;container=black;input=blue' --cmd Hyprland'';
         user = "greeter";
       };
     };
